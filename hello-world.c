2a5579cc7a3c32494c400beec4e17c5d5fbf78aa
Pejman Moghadam <pmoghadam@yahoo.com>
2024-11-26 14:35:40 +0330

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,12 @@
+<code C>
+// gcc hello-world.c buffer*.c str_*.c byte_*.c error*.c
+
+#include "exit.h"
+#include "buffer.h"
+
+int main()
+{
+    buffer_putsflush(buffer_1,"Hello World!\n");
+    _exit(0);
+}
+</code>
